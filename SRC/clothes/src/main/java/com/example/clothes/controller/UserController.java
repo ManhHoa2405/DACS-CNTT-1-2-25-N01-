6ae27d5c94c4cc954f22a7da5de45699d78d8f58
Nguyễn Mạnh Hòa <23010254@st.phenikaa-uni.edu.vn>
2026-01-20 09:50:46 +0700

    full code phần quản lí khách hàng

--- SRC/clothes/src/main/java/com/example/clothes/controller/UserController.java
+++ SRC/clothes/src/main/java/com/example/clothes/controller/UserController.java
@@ -1,16 +1,16 @@
 package com.example.clothes.controller;
-import com.example.clothes.service.UserService;
-
-import jakarta.servlet.http.HttpSession;
-
-import com.example.clothes.model.User;
-import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.clothes.model.User;
+import com.example.clothes.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
